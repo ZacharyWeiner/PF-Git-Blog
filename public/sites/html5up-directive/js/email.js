13c0b9e4abad20d6e61f6aff2b8664f5624d65b5
ZacharyWeiner <zach@cloud.com>
2015-03-10 20:35:34 -0400

    working message submission

--- public/sites/html5up-directive/js/email.js
+++ public/sites/html5up-directive/js/email.js
@@ -3,12 +3,26 @@ $(function(){
 
 	Parse.initialize("F9JwBgjiIktkVlGLlIHiFn7NPefiSZp9YqSkVfjc","KRo9teT0uHQHvqFHYtSJr36AuN2w9kbPhV4qWFfw");
 
-	var Message = Parse.Object.extend("Message");
-	var message = new Message();
+	
 
 	var MessageView = Parse.View.extend({
-
 		template : Handlebars.compile($('#message-tpl').html()),
+		events : {
+			'submit .form-message': 'sendMessage'
+		},
+		sendMessage : function(e){
+			e.preventDefault();
+			var data = $(e.target).serializeArray(),
+			 name = data[0].value,
+			 email = data[1].value, 
+			 text = data[2].value;
+
+			 var Message = Parse.Object.extend("Message");
+			var message = new Message();
+			message.save({"name":name, "email":email, "text":text}).then(function(message){
+				alert('message sent!');
+			});
+		},
 		render : function(){
 			this.$el.html(this.template());
 		}
@@ -17,4 +31,6 @@ $(function(){
 	var messageView = new MessageView();
 	messageView.render();
 	$('.messageContainer').html(messageView.el);
+
+
 });
